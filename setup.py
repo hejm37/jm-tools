235850cf0ead5b748c5e691849e110f6ebceadcd
hejiamin <hejiamin@sensetime.com>
2020-03-11 13:06:19 +0800

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='jmtools',
       version='0.101',
       description='Personal python toolbox',
       url='#',
       author='Jiamin',
-      packages=['jmtools', 'jmtools.utils', 'jmtools.cv'],
+      packages=find_packages(),
       zip_safe=False)
